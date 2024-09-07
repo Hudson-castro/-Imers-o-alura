d3e21cbd94b9b8ae37211056b38539f8624d7bc7
Hudson Castro <hudsoncastro24@gmail.com>
2024-09-07 10:49:28 -0300

    Primeiro commit do meu projeto

---
+++ dados.js
@@ -0,0 +1,24 @@
+const dados = [
+    {
+
+        titulo: "Legião Urbana ",
+        descricao: "Legião Urbana foi uma banda brasileira de rock formada em 1982, em Brasília, por Renato Russo e Marcelo Bonfá, que contou com Dado Villa-Lobos e Renato Rocha em sua formação mais conhecida.",
+
+        link: "https://pt.wikipedia.org/wiki/Legi%C3%A3o_Urbana",
+        tags: "Urbana rock nacional",
+    },
+    {
+
+
+        titulo: "Titãs ",
+        descricao: "Titãs é uma banda de rock formada na cidade de São Paulo, Brasil em 1982. Embora originalmente tocassem pop-rock alternativo em seus primórdios, o grupo também já utilizou diversos outros gêneros",
+        link: "https://pt.wikipedia.org/wiki/Tit%C3%A3s_(banda)",
+        tags: "nacional classico",
+    },
+    {
+        titulo: "Os Mutantes",
+        descricao: "Os Mutantes é uma banda brasileira de rock psicodélico formada durante o Movimento Tropicalista, no ano de 1966, em São Paulo, por Arnaldo Baptista, Rita Lee e Sérgio Dias. Também participaram do grupo Liminha e Dinho Leme",
+        link: "https://pt.wikipedia.org/wiki/Os_Mutantes",
+        tags: "",
+    }
+];
